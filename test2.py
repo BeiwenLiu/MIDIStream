3024f84e20a5b3a6da8d5e2e1c5959d95b2c7c9f
Beiwen Liu <bliu85@gatech.edu>
2017-05-24 01:18:17 -0700

    Replace with Metronome class

--- test2.py
+++ test2.py
@@ -1,13 +1,14 @@
 import EventDispatcher
-import WhoAsk
+import Metronome
 import WhoRespond
 # Create and instance of event dispatcher
 dispatcher = EventDispatcher.EventDispatcher()
 
 # Create an instance of WhoAsk class and two instance of WhoRespond class
-who_ask = WhoAsk.WhoAsk( dispatcher )
+metronome = Metronome.Metronome( dispatcher )
+
 who_responde1 = WhoRespond.WhoRespond( dispatcher )
 who_responde2 = WhoRespond.WhoRespond( dispatcher )
 
 # WhoAsk ask :-)
-who_ask.ask()
+metronome.startTick()
