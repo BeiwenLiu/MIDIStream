6242afc7eaee060c7f49dabc13a22dc054dfb20e
Beiwen Liu <bliu85@gatech.edu>
2017-05-25 01:22:42 -0700

    initialize converter and analyze measure after each signal call out

--- ScoreParser.py
+++ ScoreParser.py
@@ -1,12 +1,17 @@
 import MyEvent
+from music21 import *
 
 class ScoreParser( object ):
     """
     Second class who respond to ASK events
     """
-    def __init__(self, event_dispatcher):
+
+    def __init__(self, event_dispatcher, sheetFile):
         # Save event dispatcher reference
+        self.measureCounter = 0
         self.event_dispatcher = event_dispatcher
+        self.sheetFile = sheetFile
+        self.c = self.initializeSheets()
 
         # Listen for ASK event type
         self.event_dispatcher.add_event_listener(
@@ -17,4 +22,20 @@ class ScoreParser( object ):
         """
         Event handler for ASK event type
         """
-        
+        self.measureCounter += 1
+        self.analyze(self.measureCounter)
+
+
+    def initializeSheets(self):
+        return converter.parse(self.sheetFile)
+
+    def analyze(self, measureCounter):
+        m = self.c.parts[0].measure(measureCounter)
+
+        print "measure : ", measureCounter
+        for notes in m.flat:
+            if type(notes) is note.Note or type(notes) is chord.Chord:
+                print notes
+                print notes.offset
+
+
--- test2.py
+++ test2.py
@@ -1,13 +1,15 @@
 import EventDispatcher
 import Metronome
 import ScoreParser
+
+sheetFile = 'page1.xml'
 # Create and instance of event dispatcher
 dispatcher = EventDispatcher.EventDispatcher()
 
 # Create an instance of WhoAsk class and two instance of WhoRespond class
 metronome = Metronome.Metronome( dispatcher )
 
-scoreParser = ScoreParser.ScoreParser( dispatcher )
+scoreParser = ScoreParser.ScoreParser(dispatcher,sheetFile)
 
 # WhoAsk ask :-)
 metronome.startTick()
